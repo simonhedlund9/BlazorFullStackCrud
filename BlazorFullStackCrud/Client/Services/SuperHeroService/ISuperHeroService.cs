bf340088ef132b5ab535f27fe1b616a629c4da63
Simon Hedlund <sihe0008@student.hv.se>
2022-05-04 20:01:18 +0200

    Added create update delete on server and client aswell as created the database with entityframework

--- BlazorFullStackCrud/Client/Services/SuperHeroService/ISuperHeroService.cs
+++ BlazorFullStackCrud/Client/Services/SuperHeroService/ISuperHeroService.cs
@@ -9,5 +9,8 @@
         Task GetSuperHeroes();
 
         Task<SuperHero> GetSingleHero(int id);
+        Task CreateHero (SuperHero hero);
+        Task UpdateHero (SuperHero hero);
+        Task DeleteHero (int id);   
     }
 }
--- BlazorFullStackCrud/Client/Services/SuperHeroService/SuperHeroService.cs
+++ BlazorFullStackCrud/Client/Services/SuperHeroService/SuperHeroService.cs
@@ -1,17 +1,39 @@
-﻿using System.Net.Http.Json;
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 
 namespace BlazorFullStackCrud.Client.Services.SuperHeroService
 {
     public class SuperHeroService : ISuperHeroService
     {
         private readonly HttpClient _http;
+        private readonly NavigationManager _navigationManager;
 
-        public SuperHeroService(HttpClient http)
+        public SuperHeroService(HttpClient http, NavigationManager navigationManager)
         {
             _http = http;
+            _navigationManager = navigationManager;
         }
         public List<SuperHero> Heroes { get; set ; } = new List<SuperHero> ();
-        public List<Comic> Comics { get ; set;} = new List<Comic> ();   
+        public List<Comic> Comics { get ; set;} = new List<Comic> ();
+
+        public async Task CreateHero(SuperHero hero)
+        {
+            var result = await _http.PostAsJsonAsync("api/superhero", hero);
+            await SetHeroes(result);
+        }
+
+        private async Task SetHeroes(HttpResponseMessage result)
+        {
+            var response = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
+            Heroes = response;
+            _navigationManager.NavigateTo("superheroes");
+        }
+
+        public async Task DeleteHero(int id)
+        {
+            var result = await _http.DeleteAsync($"api/superhero/{id}");
+            await SetHeroes(result);
+        }
 
         public async Task GetComics()
         {
@@ -34,9 +56,14 @@ namespace BlazorFullStackCrud.Client.Services.SuperHeroService
         {
             var result = await _http.GetFromJsonAsync<List<SuperHero>>("api/superhero");
             if (result != null)
-   
                 Heroes = result;
             
+        }
+
+        public async Task UpdateHero(SuperHero hero)
+        {
+            var result = await _http.PutAsJsonAsync($"api/superhero/{hero.Id}", hero);
+            await SetHeroes(result);
         }
     }
 }
